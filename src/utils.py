f1a383e1dd5a8f2da25fd1732d666fa334bc6b74
Сидоров Леонид Станиславович <leonid.sidorov@sbermarket.ru>
2023-06-18 22:25:47 +0300

    localization fix

--- src/utils.py
+++ src/utils.py
@@ -26,13 +26,13 @@ def format_wether_message(weather_attrs: Dict, location: Optional[str] = None) -
     if location is None:
         location = 'location'
     message = _("The weather in") + " " + str(location) + " " + _("is") + " " + \
-              weather_attrs['status'].decode('ascii') + \
-              '\n' + _("The temperature is ") + " " + \
-              str(int((weather_attrs['temp'].decode('ascii')))) + \
-              ngettext("degree", "degrees", abs(int(weather_attrs['temp'].decode('ascii')))) + \
+              weather_attrs['status'] + \
+              '\n' + _("The temperature is ") + \
+              str(int((weather_attrs['temp']))) + " " + \
+              ngettext("degree", "degrees", abs(int(weather_attrs['temp']))) + \
               '\n' + _("feels like") + " " + \
-              str(int(weather_attrs['temp_feels'].decode('ascii'))) + \
-              ngettext("degree", "degrees", abs(int(weather_attrs['temp_feels'].decode('ascii')))) + \
-              '\n' + _("Sun sets at") + " " + weather_attrs['sunset'].decode('ascii') + " " +\
-              _("and rises at") + weather_attrs['sunrise'].decode('ascii')
+              str(int(weather_attrs['temp_feels'])) + " " + \
+              ngettext("degree", "degrees", abs(int(weather_attrs['temp_feels']))) + \
+              '\n' + _("Sun sets at") + " " + weather_attrs['sunset'] + " " +\
+              _("and rises at") + " " + weather_attrs['sunrise']
     return message
